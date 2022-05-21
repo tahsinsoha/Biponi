2c7eddb8cbca60c5ad30c26411366b3fe5a560bd
Sabiha Tahsin Soha <soha97368@gmail.com>
2022-05-21 13:11:24 +0600

    initial rest api for goals

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,12 @@
 console.log('Hello World')
 
  const express = require("express");
+ const colors = require('colors')
  const dotenv = require('dotenv').config()
  const {errorHandler} = require('./middleware/errorMiddleware')
+ const connectDB = require('./config/db')
  const port = 5000;
+ connectDB()
  const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
@@ -13,7 +16,6 @@ app.use(errorHandler)
 // app.use("/posts", () => {
 //     console.log("This is a middleware running");
 //   });
-
 // //Routes
 // app.get("/api/goals", (req, res) => {
 //   res.send("We are api");
