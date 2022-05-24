08f420df4c1653c831d7a07a78b7f3de15b5c836
Aritra Mazumder <Aritra741@gmail.com>
2022-05-24 13:04:07 +0600

    Photos upload done

--- Backend/server.js
+++ Backend/server.js
@@ -35,23 +35,41 @@ app.use(cors());
 //   });
 
 
-let gfs;
-const conn = mongoose.connection;
-conn.once("open", function () {
+
+ const conn = mongoose.connection;
+// conn.once("open", function () {
+//     gfs = Grid(conn.db, mongoose.mongo);
+//     gfs.collection("photos");
+// });
+
+let gfs, gridfsBucket;
+conn.once('open', () => {
+    gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
+        bucketName: 'photos'
+    });
+
     gfs = Grid(conn.db, mongoose.mongo);
-    gfs.collection("photos");
-});
+    gfs.collection('photos');
+})
 
 app.use("/file", upload);
 
 // media routes
+
 app.get("/file/:filename", async (req, res) => {
     try {
-        const file = await gfs.files.findOne({ filename: req.params.filename });
-        const readStream = gfs.createReadStream(file.filename);
+         const file = await gfs.files.findOne({ filename: req.params.filename });
+         console.log(file)
+        // const readStream = gfs.createReadStream(file.filename);
+
+        // // readStream.pipe(res);
+        // res.send(readStream)
+
+        const readStream = gridfsBucket.openDownloadStream(file._id);
+        console.log(file)
         readStream.pipe(res);
     } catch (error) {
-        res.send("not found");
+        console.log(error);
     }
 });
 
