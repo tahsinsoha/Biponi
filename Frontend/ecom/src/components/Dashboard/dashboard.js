4e5661f446084f05c4b411ac215fdd6086398888
Sabiha Tahsin Soha <soha97368@gmail.com>
2022-08-26 19:26:56 +0600

    dashboard changed

---
+++ Frontend/ecom/src/components/Dashboard/dashboard.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import App from "../../Daktarbari-admin-dashboard/src/app/App"
+
+function Dashboard()
+{
+    return (
+        <div>
+            <App />
+        </div>
+    )
+}
+
+export default Dashboard;
