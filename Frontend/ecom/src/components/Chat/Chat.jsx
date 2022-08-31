0744b30a6fff1d6d727b80bc4ca9b347d22bc16a
Aritra Mazumder <Aritra741@gmail.com>
2022-08-31 07:10:18 +0600

    io fixed

--- Frontend/ecom/src/components/Chat/Chat.jsx
+++ Frontend/ecom/src/components/Chat/Chat.jsx
@@ -11,7 +11,7 @@ if( localStorage.getItem('user')=='629ebb2ee9a4d3fbd9dff488' )
   return (
     <ChatEngine
       height="100vh"
-      projectID="f15ccf03-af0e-469c-aecc-9e900c5add11"
+      projectID="97e77d8c-2299-4384-bcad-50d2c53a51be"
       userName="admin"
       userSecret="123456"
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
@@ -21,8 +21,8 @@ if( localStorage.getItem('user')=='629ebb2ee9a4d3fbd9dff488' )
 return (
 <ChatEngine
       height="100vh"
-      projectID="f15ccf03-af0e-469c-aecc-9e900c5add11"
-      userName="Soha"
+      projectID="97e77d8c-2299-4384-bcad-50d2c53a51be"
+      userName="soha"
       userSecret="123456"
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
       onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
